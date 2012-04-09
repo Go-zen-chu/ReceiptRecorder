837b4db5fd9371ede5ae15795e0c4b1382fe8b5b
Akira Masuda <go.zen.chu.during.the.morning@gmail.com>
2012-04-09 23:33:07 +0900

    added store total payment

--- receipt/admin.py
+++ receipt/admin.py
@@ -5,9 +5,10 @@ from django.contrib import admin
 from django.http import HttpResponse
 from django.utils.functional import update_wrapper
 from django.views.generic.simple import direct_to_template
+from django.db.models import Sum
 
 class ReceiptAdmin(admin.ModelAdmin):
-    list_display = ("date_of_purchase","total_payment","memo","payment_user","common_payment","keichi_expense","akira_expense")
+    list_display = ("date_of_purchase","total_payment","memo","payment_user","common_payment","keichi_expense","akira_expense","shop")
     # 以下、日付の詳細なフィルター
     # list_filter = ["date_of_purchase"]
     date_hierarchy = "date_of_purchase"
@@ -17,6 +18,7 @@ class ReceiptAdmin(admin.ModelAdmin):
             <li><a href="/admin/receipt/receipt/">レシート確認画面</a></li><br>
             <li><a href="/admin/receipt/receipt/add/">レシート追加画面</a></li><br>
             <li><a href="/admin/receipt/receipt/monthly_summary/">月別合計</a></li><br>
+            <li><a href="/admin/receipt/shop/monthly_summary/">店別合計</a></li><br>
             <li><a href="/admin/">管理サイト</a></li><br>''')
 
     def compute_summary_of_month(self, request):
@@ -74,4 +76,45 @@ class ReceiptAdmin(admin.ModelAdmin):
     
 # Receiptクラスをadminサイトに反映させる
 admin.site.register(Receipt, ReceiptAdmin)
-admin.site.register(Shop)
+
+
+class ShopAdmin(admin.ModelAdmin):
+    # 各店舗の合計を計算し、表示する
+    def compute_summary_of_store(self, request):
+        first_date = Receipt.objects.all().order_by("date_of_purchase")[0].date_of_purchase
+        last_date = Receipt.objects.all().order_by("-date_of_purchase")[0].date_of_purchase
+        year = first_date.year
+        month = first_date.month
+        date_list = []
+        shop_list = []
+        total_payment_list = []
+        while True:
+            for receipt in Receipt.objects.filter(date_of_purchase__year=year,
+                                                date_of_purchase__month=month).select_related(depth=1).values("shop__name").annotate(total=Sum("total_payment")):
+                shop_list.append(receipt["shop__name"])
+                total_payment_list.append(receipt["total"])
+                
+            date_list.append((year,month))
+            
+            if last_date.year == year and last_date.month == month:
+                break
+            else:
+                month += 1
+            
+            if month == 13:
+                month = 1
+                year += 1
+                
+        context = { 'title': u"店舗別支払い", 'date_list': date_list, 'shop_list': shop_list,
+                     'total_payment_list': total_payment_list, }
+        return direct_to_template(request, "shops_monthly_reports.html", context)
+
+    # adminサイトにホームページや月別合計のページを登録するのに使う
+    def get_urls(self):
+        urls = super(ShopAdmin,self).get_urls()
+        my_urls = patterns('',
+                    (r'^monthly_summary/$', self.admin_site.admin_view(self.compute_summary_of_store)),
+        )
+        return my_urls + urls
+
+admin.site.register(Shop,ShopAdmin)
--- receipt/models.py
+++ receipt/models.py
@@ -1,12 +1,11 @@
 # encoding: utf-8
+from datetime import datetime
 from django.contrib import admin
-from django.db import models
 from django.core.exceptions import ValidationError
-from datetime import datetime    
+from django.db import models
 
 class Shop(models.Model):
-    name = models.CharField(u'店名', max_length=128, blank=False)
-
+    name = models.CharField(u'店名', max_length=128)
     def __unicode__(self):
         return "%s" % (self.name)
 
@@ -20,7 +19,7 @@ class Receipt(models.Model):
     keichi_expense = models.IntegerField(u"慧智自腹の分", blank=True, default=0)
     akira_expense = models.IntegerField(u"彬自腹の分", blank=True, default=0)
     memo = models.CharField(u"メモ", max_length=140, blank=True)
-    shop = models.ForeignKey(Shop, blank=False)
+    shop = models.ForeignKey(Shop, default=1, verbose_name=u"店名")
     
     def __unicode__(self):
         return "[%s]%s" % (self.date_of_purchase, self.memo)
@@ -34,6 +33,5 @@ class Receipt(models.Model):
             raise ValidationError('慧智自腹分は0以上の値を入力してください。')
         if self.akira_expense < 0:
             raise ValidationError('彬自腹分は0以上の値を入力してください。')
-
         if self.total_payment < self.keichi_expense + self.akira_expense:
             raise ValidationError('自腹分の合計が合計金額を超えています。')
